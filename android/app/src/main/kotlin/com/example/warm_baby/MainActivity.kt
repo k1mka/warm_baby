5f5c673921feb6a2f9ba905f9e2871f6ddd4bb45
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-07-19 13:46:33 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/warm_baby/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.warm_baby
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  List<String> orders = ['Заказ №1', 'Заказ №2', 'Заказ №3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text('W.a.r.m Baby'),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            ...orders
+                .map((e) => Text(
+                      e,
+                      style: TextStyle(fontSize: 50),
+                    ))
+                .toList(),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.search,
+                    size: 34,
+                  ),
+                  onPressed: () {
+                    print('поиск');
+                  },
+                ),
+                const Text(
+                  'Search',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Montserrat',
+                      color: Colors.black),
+                ),
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black12,
+          onPressed: () {},
+          child: const Text('add'),
+        ),
+      ),
+    );
+  }
+}
